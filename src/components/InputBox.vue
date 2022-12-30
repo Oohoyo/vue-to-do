ec7ed10485f5c9dc4a32ec7af037618a027896fd
rmne <faaa@live.com>
2022-12-30 22:55:21 +0800

    1 file modified

--- src/components/InputBox.vue
+++ src/components/InputBox.vue
@@ -44,21 +44,21 @@ export default {
       @keyup.enter="addTodo"
       v-model="inputValue"
     />
-    &nbsp;
     <button @click="addTodo">新增</button>
   </div>
 </template>
 
 <style scoped>
-.todo-header input {
+input {
   width: 80%;
   height: 28px;
   font-size: 14px;
   border: 1px solid #ccc;
   border-radius: 4px;
   padding: 4px 7px;
+  margin-right: 20px;
 }
-.todo-header input:focus {
+input:focus {
   outline: none;
   border-color: rgba(82, 168, 236, 0.8);
   box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075),
